7b9c01f7a194812f10ba65d39a8ece4a541836f5
kushik <koushikkuthuru@gmail.com>
2024-12-09 22:33:34 +0530

    first commit

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,11 +1,24 @@
-function startChat() {
+function toggleChat() {
     const chatContainer = document.getElementById('chat-container');
-    chatContainer.style.display = 'flex';
-}
+    const startChatButton = document.getElementById('start-chat-button');
 
-function closeChat() {
-    const chatContainer = document.getElementById('chat-container');
-    chatContainer.style.display = 'none';
+    if (chatContainer.style.display === 'flex') {
+        // Close the chat
+        chatContainer.style.display = 'none';
+        startChatButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+            </svg>`;
+        startChatButton.classList.remove('close'); // Remove close class
+    } else {
+        // Open the chat
+        chatContainer.style.display = 'flex';
+        startChatButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+            </svg>`;
+        startChatButton.classList.add('close'); 
+    }
 }
 
 function sendMessage() {
